6e88452c925e4e9fbdf9a611b0da7ba97fc3be6a
Daniel Aspect <dnh77227@gmail.com>
2023-06-22 23:04:27 -0300

    fixed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MontyHall;
+
+public class Program
+{
+    public static void Main()
+    {
+        var montyHall = new MontyHallStatistics();
+        List<Tuple<int, string>> possibilidades;
+
+        for (int i = 0; i < 100; i++)
+        {
+            possibilidades = montyHall.GerarPossibilidades();
+
+            var partidasGanhas = possibilidades.Count(p => p.Item2 == "Ganhou");
+            var partidasPerdidas = possibilidades.Count(p => p.Item2 == "Perdeu");
+            double razao = (double)partidasGanhas / partidasPerdidas;
+
+            //foreach (var possibilidade in possibilidades)
+            //    Console.WriteLine(possibilidade);
+            string[] resultado = new string[2];
+            resultado[0] = "------------------------------------";
+            resultado[1] = $"Partidas ganhas: {partidasGanhas} \nPartidas perdidas: {partidasPerdidas} \nRazão: {razao}";
+
+
+            Console.WriteLine(resultado[1]);
+            SalvarResultados(resultado);
+        }
+    }
+
+    public static void SalvarResultados(string[] resultados)
+    {
+        File.AppendAllLines("resultados.txt", resultados);
+    }
+}
