140afcf88be51f3f0b7a1e40d231ecfe7e91c0a0
Daniel Aspect <dnh77227@gmail.com>
2023-06-22 21:50:38 -0300

    updated game

--- MontyHallGame.cs
+++ MontyHallGame.cs
@@ -6,16 +6,16 @@ public class MontyHallGame
     private int portaEscolhida; // a porta que o jogador escolheu.
     private int portaRevelada; // a porta que o apresentador abriu.
 
-    public MontyHallGame()
+    public void CriarPortas()
     {
         // Escolhe qual vai ser a porta premiada.
-        portaPremiada = new Random().Next(1, 3);
+        this.portaPremiada = new Random().Next(1, 3);
 
         // Escolhe uma porta.
-        portaEscolhida = new Random().Next(1, 3);
+        this.portaEscolhida = new Random().Next(1, 3);
 
         // Abre uma porta.
-        portaRevelada = new[] { 1, 2, 3 }.Except(new[] { portaEscolhida, portaPremiada }).First();
+        this.portaRevelada = new[] { 1, 2, 3 }.Except(new[] { portaEscolhida, portaPremiada }).First();
         // Note que a função except faz com que a porta revelada .
         // não seja a porta escolhida pelo usuário ou a porta.
         // que contém o prêmio.
