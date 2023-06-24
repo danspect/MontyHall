111db02c2e5c1b4c80564ca4c93e5c9027cbdd04
Daniel Aspect <dnh77227@gmail.com>
2023-06-24 17:53:15 -0300

    reformulated experiment

--- Program.cs
+++ Program.cs
@@ -2,34 +2,62 @@
 
 public class Program
 {
-    public static void Main()
+    // Este valor que controla se o jogador deve ou não mudar de porta.
+    private static bool trocarPorta = false;
+
+    public static async Task SalvarResultados(string[] resultados, bool _trocarPorta)
+        => await File.AppendAllLinesAsync($"resultados-{_trocarPorta}.csv", resultados);
+
+    private static int CriarPorta()
+        => new Random().Next(1, 3);
+
+    /// <summary>
+    ///     Esta função é o cérebro do jogo. Dependendo do experimento, podemos fazer o 
+    ///  "jogador" trocar ou não de porta. 
+    /// </summary>
+    /// <param name="_trocarPorta"> Este parâmetro dita se o jogador vai trocar ou não de porta. </param>
+    /// <returns> Retorna se o jogador ganhou ou não a partida. </returns>
+    private static bool GerarPartida(bool _trocarPorta)
     {
-        var montyHall = new MontyHallStatistics();
-        List<Tuple<int, string>> possibilidades;
+        int portaPremiada = CriarPorta();
+        int portaEscolhida = CriarPorta();
+        int portaRevelada = CriarPorta();
 
-        for (int i = 0; i < 1000; i++)
+        do
         {
-            possibilidades = montyHall.GerarPossibilidades();
+            portaRevelada = CriarPorta();
+        } while (portaRevelada == portaPremiada || portaRevelada == portaEscolhida);
 
-            var partidasGanhas = possibilidades.Count(p => p.Item2 == "Ganhou");
-            var partidasPerdidas = possibilidades.Count(p => p.Item2 == "Perdeu");
-            double razao = (double)partidasGanhas / partidasPerdidas;
+        if (_trocarPorta)
+        {
+            do
+            {
+                portaEscolhida = CriarPorta();
+            } while (portaEscolhida == portaPremiada || portaEscolhida == portaRevelada);
+        }
 
-            //foreach (var possibilidade in possibilidades)
-            //    Console.WriteLine(possibilidade);
-            string[] resultado = new string[2];
+        return portaEscolhida == portaPremiada;
+    }
+
+    public static async Task Main()
+    {
+        bool resultado;
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            resultado = GerarPartida(trocarPorta);
+            //var partidasGanhas = possibilidades.Count(p => p.Item2 == "Ganhou");
+            //var partidasPerdidas = possibilidades.Count(p => p.Item2 == "Perdeu");
+            // double razao = (double)partidasGanhas / partidasPerdidas;
             // resultado[0] = "------------------------------------";
             // resultado[1] = $"Partidas ganhas: {partidasGanhas} \nPartidas perdidas: {partidasPerdidas} \nRazão: {razao}";
-            resultado[0] = $"{i}, {partidasGanhas}, {partidasPerdidas}, {razao}"; // Modelo para arquivo csv.
+            string[] resultadosFormatado = new string[1];
+            resultadosFormatado[0] = $"{i}, {resultado}"; 
+            // Modelo para arquivo csv.
 
 
             // Console.WriteLine(resultado[1]);
-            SalvarResultados(resultado);
+            await SalvarResultados(resultadosFormatado, trocarPorta);
         }
-    }
-
-    public static void SalvarResultados(string[] resultados)
-    {
-        File.AppendAllLines("resultados.csv", resultados);
     }
 }
