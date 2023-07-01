dd6ee134e0ccd69d13e7fbe48ddd5eab1b274d1d
Daniel Aspect <dnh77227@gmail.com>
2023-06-30 21:18:25 -0300

    Automated file creation process

--- main.py
+++ main.py
@@ -32,17 +32,24 @@ def monty_hall(trocar_porta: bool) -> bool:
     return True if primeira_porta_escolhida == porta_premiada else False
 
 
-def write_to_csv(texto: str, trocar_porta: bool, range: int) -> None:
+def write_to_csv(texto: str, trocar_porta: bool, range: int, num_arquivo: int) -> None:
     ''' Escreve os dados da partida em um arquivo .csv. '''
-    with open(f"resultados-{trocar_porta}-{range}.csv", "a") as f:
+    with open(f"resultados-{trocar_porta}-{range}-{num_arquivo}.csv", "a") as f:
         f.write(texto)
         f.close()
 
 
-trocar_porta = False
-amostra = 10_000
-write_to_csv("id, ganhou\n", trocar_porta, amostra)
-for i in range(amostra):
-    resultado: bool = monty_hall(trocar_porta)
-    resultado_formatado: str = f"{i}, {resultado}\n"
-    write_to_csv(resultado_formatado, trocar_porta, amostra)
+trocar_porta = [True, False] # Define se o jogador deve ou não trocar de porta. Não precisa ser alterado.
+amostra = 10_000 # Quantidade de partidas.
+quantidade_de_arquivos = 5
+
+# Primeiro cria arquivos em que o jogador trocou de porta e depos em que o jogador permaneceu.
+for i in trocar_porta:
+    # Cria uma quantidade n de arquivos estabelecida anteriormente.
+    for j in range(quantidade_de_arquivos):
+        write_to_csv("id, ganhou\n", i, amostra, j + 1)
+        # Cria as partidas e escreve o resultado em cada arquivo.
+        for k in range(amostra):
+            resultado: bool = monty_hall(i)
+            resultado_formatado: str = f"{k}, {resultado}\n"
+            write_to_csv(resultado_formatado, i, amostra, j + 1)
