d796ff832d8a371cba563c10c34ebfc49b99a3f2
Daniel Aspect <dnh77227@gmail.com>
2023-07-02 23:15:25 -0300

    Now it saves the data on a sqlite db

--- main.py
+++ main.py
@@ -1,51 +1,74 @@
 import random
+import sqlite3
 
-def gen_random_door() -> int:
-    ''' Retorna um número aleatório entre 1 e 3. '''
-    return random.randint(1, 3)
+class MontyHall:
 
-def monty_hall(trocar_porta: bool) -> bool:
-    ''' Cria um jogo de monty e retorna se o jogador ganhou ou não a partida. '''
-    porta_premiada: int = gen_random_door()
-    primeira_porta_escolhida: int = gen_random_door()
+    def __init__(self):
+        self.conexao = sqlite3.connect("resultados.db")
+        self.cursor = self.conexao.cursor()
 
-    # Esta parte garante que a porta escolhida pelo apresentador
-    # não será a porta escolhida pelo jogador nem a porta premiada.
-    while True:
-        porta_revelada: int = gen_random_door()
-        if porta_revelada == porta_premiada or porta_revelada == primeira_porta_escolhida:
-            continue
-        else:
-            break
+    @staticmethod
+    def gen_random_door() -> int:
+        """ Retorna um número aleatório entre 1 e 3. """
+        return random.randint(1, 3)
 
-    # Esta parte virifica se o jogador deve ou não trocar de porta.
-    if trocar_porta:
+    def monty_hall(self, trocar_porta: bool) -> bool:
+
+        """ Cria um jogo de monty e retorna se o jogador ganhou ou não a partida. """
+        porta_premiada: int = self.gen_random_door()
+        primeira_porta_escolhida: int = self.gen_random_door()
+
+        # Esta parte garante que a porta escolhida pelo apresentador
+        # não será a porta escolhida pelo jogador nem a porta premiada.
         while True:
-            segunda_porta_escolhida: int = gen_random_door()
-            if segunda_porta_escolhida == primeira_porta_escolhida or segunda_porta_escolhida == porta_revelada:
+            porta_revelada: int = self.gen_random_door()
+            if porta_revelada == porta_premiada or porta_revelada == primeira_porta_escolhida:
                 continue
             else:
-                return True if segunda_porta_escolhida == porta_premiada else False
+                break
 
-    return True if primeira_porta_escolhida == porta_premiada else False
+        # Esta parte virifica se o jogador deve ou não trocar de porta.
+        if trocar_porta:
+            while True:
+                segunda_porta_escolhida: int = self.gen_random_door()
+                if segunda_porta_escolhida == primeira_porta_escolhida or segunda_porta_escolhida == porta_revelada:
+                    continue
+                else:
+                    return True if segunda_porta_escolhida == porta_premiada else False
 
-def write_to_csv(texto: str, trocar_porta: bool, range: int, num_arquivo: int) -> None:
-    ''' Escreve os dados da partida em um arquivo .csv. '''
-    with open(f"resultados-{trocar_porta}-{range}-{num_arquivo}.csv", "a") as f:
-        f.write(texto)
-        f.close()
+        return True if primeira_porta_escolhida == porta_premiada else False
 
-trocar_porta = [True, False] # Define se o jogador deve ou não trocar de porta. Não precisa ser alterado.
-amostra = 10_000 # Quantidade de partidas.
-quantidade_de_arquivos = 5
+    def save_to_db(self, ganhou_sem_trocar: bool, ganhou_trocando: bool) -> None:
+        """ Salva os dados da partida em um banco de dados sqlite com três colunas. """
+        # Cria uma tabela chamada monty_hall SE ela não existir.
+        self.cursor.execute(
+            """CREATE TABLE IF NOT EXISTS monty_hall (
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            ganhou_sem_trocar BOOLEAN, 
+            ganhou_trocando BOOLEAN)""")
+        # Insere os dados da partida na tabela.
+        self.cursor.execute("INSERT INTO monty_hall (ganhou_sem_trocar, ganhou_trocando) VALUES (?, ?)",
+                            (ganhou_sem_trocar, ganhou_trocando))
 
-# Primeiro cria arquivos em que o jogador trocou de porta e depos em que o jogador permaneceu.
-for i in trocar_porta:
-    # Cria uma quantidade n de arquivos estabelecida anteriormente.
-    for j in range(quantidade_de_arquivos):
-        write_to_csv("id, ganhou\n", i, amostra, j + 1)
-        # Cria as partidas e escreve o resultado em cada arquivo.
-        for k in range(amostra):
-            resultado: bool = monty_hall(i)
-            resultado_formatado: str = f"{k}, {resultado}\n"
-            write_to_csv(resultado_formatado, i, amostra, j + 1)
+    def close_connection(self) -> None:
+        """ Fecha a conxão com o banco de dados. """
+        self.conexao.close()
+
+    def commit_changes(self) -> None:
+        """ Salva os dados. """
+        self.conexao.commit()
+
+# Quantidade de partidas.
+# Não é recomendado usar amostras muito grandes pois
+# os dados ficam salvos na memoria.
+amostra = 500_000
+
+mh = MontyHall()
+# Cria as partidas e salva os resultados no banco de dados.
+for i in range(amostra):
+    resultado_sem_trocar: bool = mh.monty_hall(False)
+    resultado_trocando: bool = mh.monty_hall(True)
+    mh.save_to_db(resultado_sem_trocar, resultado_trocando)
+
+mh.commit_changes()
+mh.close_connection()
