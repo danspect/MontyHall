dfd3533aa65f7c315bdf1643575f035d9970c057
Daniel Aspect <dnh77227@gmail.com>
2023-07-04 22:12:50 -0300

    fixed bugs

--- main.py
+++ main.py
@@ -9,47 +9,34 @@ class MontyHall:
         self.cursor = self.conexao.cursor()
 
     @staticmethod
-    def gen_random_door() -> int:
+    def gen_random_door(self) -> int:
         """ Retorna um número aleatório entre 1 e 3. """
         return random.randint(1, 3)
 
     def monty_hall(self, trocar_porta: bool) -> bool:
-
         """ Cria um jogo de monty e retorna se o jogador ganhou ou não a partida. """
         porta_premiada: int = self.gen_random_door()
         primeira_porta_escolhida: int = self.gen_random_door()
 
         # Esta parte garante que a porta escolhida pelo apresentador
         # não será a porta escolhida pelo jogador nem a porta premiada.
-
-def gen_random_door() -> int:
-    ''' Retorna um número aleatório entre 1 e 3. '''
-    return random.randint(1, 3)
-
-def monty_hall(trocar_porta: bool) -> bool:
-    ''' Cria um jogo de monty e retorna se o jogador ganhou ou não a partida. '''
-    porta_premiada: int = gen_random_door()
-    primeira_porta_escolhida: int = gen_random_door()
-
-    # Esta parte garante que a porta escolhida pelo apresentador
-    # não será a porta escolhida pelo jogador nem a porta premiada.
-    while True:
-        porta_revelada: int = gen_random_door()
-        if porta_revelada == porta_premiada or porta_revelada == primeira_porta_escolhida:
-            continue
-        else:
-            break
-
-    # Esta parte virifica se o jogador deve ou não trocar de porta.
-    if trocar_porta:
         while True:
             porta_revelada: int = self.gen_random_door()
             if porta_revelada == porta_premiada or porta_revelada == primeira_porta_escolhida:
                 continue
             else:
-                return True if segunda_porta_escolhida == porta_premiada else False
-              
-    return True if primeira_porta_escolhida == porta_premiada else False
+                break
+
+        # Esta parte virifica se o jogador deve ou não trocar de porta.
+        if trocar_porta:
+            while True:
+                segunda_porta_escolhida: int = self.gen_random_door()
+                if segunda_porta_escolhida == primeira_porta_escolhida or segunda_porta_escolhida == porta_revelada:
+                    continue
+                else:
+                    return True if segunda_porta_escolhida == porta_premiada else False
+
+        return True if primeira_porta_escolhida == porta_premiada else False
 
     def save_to_db(self, ganhou_sem_trocar: bool, ganhou_trocando: bool) -> None:
         """ Salva os dados da partida em um banco de dados sqlite com três colunas. """
@@ -70,6 +57,7 @@ def monty_hall(trocar_porta: bool) -> bool:
     def commit_changes(self) -> None:
         """ Salva os dados. """
         self.conexao.commit()
+
 
 # Quantidade de partidas.
 # Não é recomendado usar amostras muito grandes pois
