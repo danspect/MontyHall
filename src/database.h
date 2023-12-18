cbff02859ada921fe669f01d57d96ea6b865b6e1
Daniel Aspect <daniel_aspect@proton.me>
2023-12-17 23:21:33 -0300

    fixed header

--- src/database.h
+++ src/database.h
@@ -1,3 +1,6 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
 #include <sqlite3.h>
 #include <stdbool.h>
 
@@ -6,10 +9,9 @@ typedef struct {
     sqlite3_stmt *stmt;
 } MontyHallDB;
 
-void init_db(MontyHallDB *mh)
+void init_db(MontyHallDB *mh);
+void save_to_db(MontyHallDB *mh, bool ganhou_sem_trocar, bool ganhou_trocando);
+void commit_changes(MontyHallDB *mh);
+void close_connection(MontyHallDB *mh);
 
-void save_to_db(MontyHallDB *mh, bool ganhou_sem_trocar, bool ganhou_trocando) 
-
-void commit_changes(MontyHallDB *mh) 
-
-void close_connection(MontyHallDB *mh) 
+#endif  // DATABASE_H
