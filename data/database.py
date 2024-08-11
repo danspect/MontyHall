4272d158d17486ca7e05338b01bd2496e94b2e9a
Daniel Lima <dnlima77237@gmail.com>
2024-08-11 18:31:57 -0300

    updated models

--- data/database.py
+++ data/database.py
@@ -1,6 +1,20 @@
 from peewee import *
-from models import *
+from models.game import *
+from config.settings import db_name
 
-class Database():
+class Database:
 
-    pass
+    def __init__(self, database_name: str) -> None:
+        self.connection = SqliteDatabase(database_name)
+        self.connection.connect()
+
+    def commit_changes(self):
+        self.connection.commit()
+
+    def close_connection(self):
+        self.connection.close()
+
+if __name__ == "__main__":
+    database = Database(db_name)
+    database.connection.create_tables([Game])
+    _game = Game.create(1, 0, 1)
--- models/game.py
+++ models/game.py
@@ -1,8 +1,8 @@
 from peewee import *
 
+
 class Game(Model):
 
-    def __init__(self, win_changing: int, win_not_changing: int, id: int) -> None:
-        id = IntegerField
-        win_changing = IntegerField()
-        win_changing = IntegerField()
+    id = IntegerField
+    win_changing = IntegerField
+    win_changing = IntegerField()
