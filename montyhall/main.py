a103a7de33ac14e4cbcde12d071573df273daf5b
Daniel Lima <dnlima77237@gmail.com>
2024-09-04 05:39:34 -0300

    Include plot function

--- montyhall/main.py
+++ montyhall/main.py
@@ -1,4 +1,5 @@
 from database import Database, Game
+from data_analysis import plot
 from montyhallgame import MontyHallGame
 from settings import amostra, db_name
 
@@ -9,4 +10,8 @@ for i in range(amostra):
     print(f"Iteration: {i}", end='\r')
     db.insert_data(Game(mh.monty_hall(True), mh.monty_hall(False)))
 
+data = db.get_all_games()
+
 db.close_connection()
+
+plot(data)
