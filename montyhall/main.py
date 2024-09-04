0382728a9748d694738b14a25bab495fadddf57c
Daniel Lima <dnlima77237@gmail.com>
2024-09-04 04:54:25 -0300

    Created game logic

--- montyhall/main.py
+++ montyhall/main.py
@@ -1,2 +1,12 @@
-from .montyhall import Montyhall
+from database import Database, Game
+from montyhallgame import MontyHallGame
+from settings import amostra, db_name
+
+db = Database(db_name)
+mh = MontyHallGame()
+
+for i in range(500):
+    db.insert_data(Game(mh.monty_hall(True), mh.monty_hall(False)))
+
+db.close_connection()
 
