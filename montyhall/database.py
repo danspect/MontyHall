92df21653dbc2c00d12462f95085511dee652806
Daniel Lima <dnlima77237@gmail.com>
2024-09-04 04:27:26 -0300

    Fixed bugs

--- montyhall/database.py
+++ montyhall/database.py
@@ -1,5 +1,4 @@
 import sqlite3
-from unittest import result
 
 
 class Game:
@@ -11,12 +10,8 @@ class Game:
 class Database:
 
     def __init__(self, db_name: str) -> None:
-        self.db_name = db_name
-        self.connection: sqlite3.Connection
-        self.cursor = self.connection.cursor()
-        self.create_table()
-
-    def create_table(self) -> None:
+        self.database_connection: sqlite3.Connection = sqlite3.connect(db_name)
+        self.cursor: sqlite3.Cursor = self.database_connection.cursor()
         self.cursor.execute(
             """CREATE TABLE IF NOT EXISTS Game(
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,23 +22,30 @@ class Database:
 
     def insert_data(self, game: Game) -> None:
         self.cursor.execute(
-            "INSERT INTO Game VALUES (?, ?)", (game.won_changing, game.won_not_changing)
+            "INSERT INTO Game(won_changing, won_not_changing) VALUES (?, ?)",
+            (game.won_changing, game.won_not_changing),
         )
-        self.connection.commit()
+        self.database_connection.commit()
 
     def get_all_games(self) -> list[tuple[int, int, int]]:
-        result = self.cursor.execute(
-            "SELECT * FROM Game"
-        )
+        result = self.cursor.execute("SELECT * FROM Game")
         return result.fetchall()
-    
+
     def get_game_by_id(self, id: int) -> tuple[int, int]:
         result = self.cursor.execute(f"SELECT id FROM Game WHERE id == {id}")
         return result.fetchone()
 
-    def __enter__(self) -> sqlite3.Connection:
-        self.connection = sqlite3.connect(self.db_name)
-        return self.connection
+    # def __enter__(self) -> sqlite3.Connection:
+    #    self.database_connection = sqlite3.connect(self.db_name)
+    #    return self.database_connection
 
-    def __exit__(self) -> None:
-        self.connection.close()
+    # def __exit__(self, exc_type, exc_value, traceback) -> None:
+    #    self.database_connection.close()
+
+
+if __name__ == "__main__":
+    db = Database("test.db")
+
+    for i in range(10):
+        db.insert_data(Game(True, False))
+    print(db.get_all_games())
